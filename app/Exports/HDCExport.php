6a225aeb12f6043117ec030ad805fda119b46436
James Polo <polojames552@gmail.com>
2022-05-25 02:30:52 +0800

    Excel export files

---
+++ app/Exports/HDCExport.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Exports;
+
+use Maatwebsite\Excel\Concerns\FromCollection;
+
+class HDCExport implements FromCollection
+{
+    /**
+    * @return \Illuminate\Support\Collection
+    */
+    public function collection()
+    {
+        //
+    }
+}
